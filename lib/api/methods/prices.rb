94930e61c1f3703398135112d73ca1f4709cab30
Arjan Scherpenisse <arjan@scherpenisse.net>
2013-09-18 16:01:30 +0200

    Remove old var-price calls; add optional inventory owner code to get_price()

--- lib/api/methods/prices.rb
+++ lib/api/methods/prices.rb
@@ -9,12 +9,14 @@ module SpaazaApi
       }
     end
 
-    def get_price(product_id, claim_key=nil)
+    def get_price(product_id, inventory_owner_code, claim_key)
       require_protected_path
-      get "#{protected_path}/get-var-price-user.json", :query => {
-        :product_id => product_id,
-        :claim_key => claim_key
-      }
+      if inventory_owner_code.blank?
+        query = {:product_id => product_id, :claim_key => claim_key}
+      else
+        query = {:inventory_owner_code => inventory_owner_code, :claim_key => claim_key}
+      end
+      get "#{protected_path}/get-var-price-user.json", :query => query
     end
 
     def get_var_price_product_info(product_id)
@@ -27,43 +29,6 @@ module SpaazaApi
       get "#{protected_path}/get-var-prices-entity.json", :query => {
         :business_id => business_id, 
         :chain_id => chain_id 
-      }
-    end
-
-    def variable_get_variableprice_auth(product_id)
-      require_protected_path
-      get "#{protected_path}/get-var-price.json", :query => {:product_id => product_id }
-    end
-
-    def variable_put_variableprice_auth (product_id, key, value)
-      require_protected_path
-      put "#{protected_path}/alter-var-price.json", :body => {
-        :product_id => product_id, 
-        key => value 
-      }
-    end
-
-    def variable_delete_variableprice_auth(var_price_id)
-      require_protected_path
-      delete "#{protected_path}/delete-var-price.json", :body => {
-        :var_price_id => var_price_id 
-      }
-    end
-
-    def variable_post_variableprice_auth(product_id, base_price_threshold, gender, weighting_gender, age_range_low, age_range_high, weighting_age_range, weighting_fb_like_parent_entity, weighting_mailing_list_parent_entity, weighting_fb_share_link, published_status)
-      require_protected_path
-      post "#{protected_path}/add-var-price.json", :body => {
-        :product_id => product_id,
-        :base_price_threshold => base_price_threshold,
-        :published_status => published_status,
-        :gender => gender,
-        :weighting_gender => weighting_gender,
-        :age_range_low => age_range_low,
-        :age_range_high => age_range_high,
-        :weighting_age_range => weighting_age_range,
-        :weighting_fb_like_parent_entity => weighting_fb_like_parent_entity,
-        :weighting_mailing_list_parent_entity => weighting_mailing_list_parent_entity,
-        :weighting_fb_share_link => weighting_fb_share_link
       }
     end
 
